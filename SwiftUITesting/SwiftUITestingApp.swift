b1648bedd195d134282d16e769864b28e8fd896f
nureddinelmas <nureddinelmas34@gmail.com>
2021-12-14 11:38:33 +0100

    Initial Commit

---
+++ SwiftUITesting/SwiftUITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestingApp.swift
+//  SwiftUITesting
+//
+//  Created by Nureddin Elmas on 2021-12-14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
